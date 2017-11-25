4d56cc40f740f096cb1b003a89208ee733736a68
Dfilian <33777081+Dfilian@users.noreply.github.com>
2017-11-25 11:14:47 +0200

    added GameState

---
+++ src/Engine/CurrGameState.java
@@ -0,0 +1,11 @@
+package Engine;
+
+public enum CurrGameState {
+    NotInitialized,
+    Initialized,
+    Started,
+    RunningHand,
+    ShowsStatistics,
+    StoppedByUser
+
+}
--- src/Engine/GameManager.java
+++ src/Engine/GameManager.java
@@ -5,9 +5,11 @@ import Engine.Players.Player;
 
 public class GameManager {
     private GameDescriptor gameDescriptor;
+    private CurrGameState stateOfGame;
 
     public GameManager(GameDescriptor descriptor) {
         gameDescriptor= descriptor;
+        stateOfGame=CurrGameState.NotInitialized;
     }
 
     public void buy(Player player, int amount)
