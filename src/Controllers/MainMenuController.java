b612825fc0dec850dd2ea868163b50f70d6b0457
Dfilian <33777081+Dfilian@users.noreply.github.com>
2017-12-21 22:04:28 +0200

    ok

--- src/Controllers/MainMenuController.java
+++ src/Controllers/MainMenuController.java
@@ -1,4 +1,26 @@
 package Controllers;
 
-public class MainMenuController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainMenuController implements Initializable{
+@FXML ComboBox styleComboBox;
+@FXML ComboBox animationComboBox;
+@FXML Button loadXmlButton;
+@FXML Button startButton;
+@FXML Label fileNameLabel;
+@FXML Button stopButton;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+            fileNameLabel.setText("Not Loaded");
+    }
 }
--- src/Controllers/MainScreenController.java
+++ src/Controllers/MainScreenController.java
@@ -1,17 +1,27 @@
 package Controllers;
 
 import Engine.GameManager;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.stage.Stage;
 
-public class MainScreenController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainScreenController implements Initializable {
+
+    private GameInfoAndActionsController gameInfoAndActionsController;
+    private GameTableController gameTableController;
+    private MainMenuController mainMenuController;
+    private PlayersTableController playersTableController;
+
     private GameManager gameManager;
     private Stage primaryStage;
 
 
-    public void setStage(Stage PrimaryStage)
-    {
-        primaryStage = PrimaryStage;
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
     }
 
     public GameManager getGameManager() {
@@ -28,5 +38,29 @@ public class MainScreenController {
 
     public void setPrimaryStage(Stage primaryStage) {
         this.primaryStage = primaryStage;
+    }
+
+    public GameInfoAndActionsController getGameInfoAndActionsController() {
+        return gameInfoAndActionsController;
+    }
+
+    public void setGameInfoAndActionsController(GameInfoAndActionsController gameInfoAndActionsController) {
+        this.gameInfoAndActionsController = gameInfoAndActionsController;
+    }
+
+    public GameTableController getGameTableController() {
+        return gameTableController;
+    }
+
+    public void setGameTableController(GameTableController gameTableController) {
+        this.gameTableController = gameTableController;
+    }
+
+    public MainMenuController getMainMenuController() {
+        return mainMenuController;
+    }
+
+    public PlayersTableController getPlayersTableController() {
+        return playersTableController;
     }
 }
